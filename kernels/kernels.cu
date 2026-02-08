0475ba2f1704a826c7430ebd24f352db1c017908
LegitBaller0243 <samjswag@gmail.com>
2026-02-08 12:55:07 -0500

    adding transpose aware, alpha-beta

--- kernels/kernels.cu
+++ kernels/kernels.cu
@@ -2,7 +2,10 @@
 
 //matmul kernel
 // each thread exists within a block and we use math to turn 1D grid to a 2D grid index of that block
-__global__ void naiveMul(float* A, float* B, float* C, int M, int K, int N) {
+__global__ void naiveMul(float* A, float* B, float* C, 
+        int M, int K, int N, 
+        float alpha, float beta) {
+
     int numX_blocks = (N + blocksize - 1) / blocksize;
 
     int blockRow = blockIdx.x / numX_blocks;
@@ -14,43 +17,91 @@ __global__ void naiveMul(float* A, float* B, float* C, int M, int K, int N) {
         float sum = 0;
         for (int k = 0; k < K; k += 1) {
             sum += A[x * K + k] * B[k * N + y];
-    }
-        C[x * N + y] = sum;
+        }
+        int idx = x * N + y;
+        if (beta == 0.0f) {
+            C[idx] = sum * (alpha);
+        } else {
+            C[idx] = sum * alpha + beta * C[idx];
+        }
     }
 }
 //tiling kernel on a 1D grid
-__global__ void tilingMul(const float* A, const float* B, float* C, int N) {
+__global__ void tilingMul(const float* A, const float* B, float* C, 
+        int M, int N, int K, 
+        float alpha, float beta) {
+            
     int numTiles = (N + blocksize - 1)/ blocksize;
     int blockRow = blockIdx.x / numTiles;
     int blockCol = blockIdx.x % numTiles;
 
-
-    int block_start_x = blockRow * blocksize;
-    int block_start_y = blockCol * blocksize;
-
-
     int r = threadIdx.y;
     int c = threadIdx.x;
+
+    int i = blockRow * blocksize + r;
+    int j = blockCol * blocksize + c; 
+
 
     __shared__ float A_block[blocksize][blocksize];
     __shared__ float B_block[blocksize][blocksize];
 
-    float sum = 0;
-    int x = block_start_x + r;
-    int y = block_start_y + c; 
-
-
-    for (int kk = 0; kk < N; kk += blocksize) {
-        A_block[r][c] = (x < N && (kk + c) < N) ? A[x * N + kk + c] : 0;
-        B_block[r][c] = (y < N && (kk + r) < N) ? B[(kk + r) * N + y] : 0;
+    float sum = 0.0f;
+    for (int kk = 0; kk < K; kk += blocksize) {
+        A_block[r][c] = (i < M && (kk + c) < K)? A[i * K + kk + c] : 0.0f;
+        B_block[r][c] = (j < N && (r + kk) < K)? B[(kk + r) * N + j] : 0.0f;
         __syncthreads();
-
+            
         for (int k = 0; k < blocksize; k ++) {
             sum += A_block[r][k] * B_block[k][c]; 
         }
         __syncthreads();
     }
-    if (x < N && y < N) C[x * N + y] = sum;
+    if (i < M && j < N) {
+        int idx = i * N + j;
+        if (beta == 0.0f) {
+            C[idx] = sum * (alpha);
+        } else {
+            C[idx] = sum * alpha + beta * C[idx];
+        }
+    }
+}
+
+__global__ void transposeTilingMul(const float* A, const float* B, float* C, 
+        int M, int N, int K, 
+        float alpha, float beta, Transpose trans) {
+    int numTiles = (N + blocksize - 1)/ blocksize;
+    int blockRow = blockIdx.x / numTiles;
+    int blockCol = blockIdx.x % numTiles;
+
+    int r = threadIdx.y;
+    int c = threadIdx.x;
+
+    int i = blockRow * blocksize + r;
+    int j = blockCol * blocksize + c; 
+
+
+    __shared__ float A_block[blocksize][blocksize];
+    __shared__ float B_block[blocksize][blocksize];
+
+    float sum = 0.0f;
+    for (int kk = 0; kk < K; kk += blocksize) {
+        A_block[r][c] = (i < M && (kk + c) < K)? A_at(A, i, K, kk, c, trans.transA): 0.0f;
+        B_block[r][c] = (j < N && (r + kk) < K)? B_at(B, j, N, kk, r, trans.transB) : 0.0f;
+        __syncthreads();
+            
+        for (int k = 0; k < blocksize; k ++) {
+            sum += A_block[r][k] * B_block[k][c]; 
+        }
+        __syncthreads();
+    }
+    if (i < M && j < N) {
+        int idx = i * N + j;
+        if (beta == 0.0f) {
+            C[idx] = sum * (alpha);
+        } else {
+            C[idx] = sum * alpha + beta * C[idx];
+        }
+    }
 }
 //batched naive matMul kernel ()
 __global__ void batchNaiveMul(const float* A, const float* B, float* C, int M, int K, int N) {
@@ -76,7 +127,7 @@ __global__ void batchNaiveMul(const float* A, const float* B, float* C, int M, i
 }
 
 
-__global__ void batchTilingMul(const float* A, const float* B, float* C, int M, int K, int N) {
+__global__ void batchStridedMul(const float* A, const float* B, float* C, int M, int K, int N, float alpha, float beta) {
     int batch = blockIdx.x;
 
     //C[b][i][j]
@@ -96,10 +147,15 @@ __global__ void batchTilingMul(const float* A, const float* B, float* C, int M, 
     __shared__ float A_block[blocksize][blocksize];
     __shared__ float B_block[blocksize][blocksize];
     float sum = 0;
+
+    const float* Ab = A + batch * M * K;
+    const float* Bb = B + batch * K * N;
+    float* Cb = C + batch * M * N;
+
     // for each block load memory into data
     for (int k = 0; k < K; k += blocksize) {
-        A_block[r][c] = (i < M && (k + c) < K)? A[batch * M * K + i * K + k + c] : 0;
-        B_block[r][c] = (j < N && (r + k) < K)? B[batch * K * N + (k + r) * N + j] : 0;
+        A_block[r][c] = (i < M && (k + c) < K)? Ab[i * K + k + c] : 0;
+        B_block[r][c] = (j < N && (r + k) < K)? Bb[(k + r) * N + j] : 0;
         __syncthreads();
 
         for (int kk = 0; kk < blocksize; kk++) {
@@ -107,8 +163,13 @@ __global__ void batchTilingMul(const float* A, const float* B, float* C, int M, 
         }
         __syncthreads();
     }
-
-    if (i < M && j < N) C[batch * M * N + i * N + j] = sum;
-
-
+    if (i < M && j < N) {
+        int idx = i * N + j;
+        if (beta == 0.0f) {
+            Cb[idx] = sum * (alpha);
+        } else {
+            Cb[idx] = sum * alpha + beta * Cb[idx];
+        }
+    }
 }
+
--- kernels/kernels.cuh
+++ kernels/kernels.cuh
@@ -5,7 +5,32 @@
 
 # define blocksize 16
 
-__global__ void naiveMul(float* A, float* B, float* C, int M, int K, int N);
-__global__ void tilingMul(const float* A, const float* B, float* C, int N);
+enum Flag : int {
+    OP_N = 0,
+    OP_T = 1
+};
+
+struct Transpose {
+    Flag transA;
+    Flag transB;
+};
+
+
+__device__ __forceinline__
+float A_at(const float* A, int i, int K, int kk, int c, Flag transA) {
+    return (transA == OP_T) 
+        ? A[(kk + c) * K + i] 
+        : A[i * K + kk + c];
+}
+__device__ __forceinline__
+float B_at(const float* B, int j, int N, int kk, int r, Flag transB) {
+    return (transB == OP_T) 
+        ? B[j * N + kk + r]
+        : B[(kk + r) * N + j];
+}
+
+__global__ void naiveMul(float* A, float* B, float* C, int M, int K, int N, float alpha, float beta);
+__global__ void tilingMul(const float* A, const float* B, float* C, int M, int N, int K, float alpha, float beta);
+__global__ void transposeTilingMul(const float* A, const float* B, float* C, int M, int N, int K, float alpha, float beta, Transpose trans);
 __global__ void batchNaiveMul(const float* A, const float* B, float* C, int M, int K, int N);
-__global__ void batchTilingMul(const float* A, const float* B, float* C, int M, int K, int N);
+__global__ void batchStridedMul(const float* A, const float* B, float* C, int M, int K, int N, float alpha, float beta);
